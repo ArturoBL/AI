b50c16b87a308b1a60e340a67877013815fce271
ArturoBL <dark.laker@gmail.com>
2023-04-13 23:11:20 -0600

    Se puso Neuralnetwork en la librería nnlib

--- BackpropagationSimple/example.py
+++ BackpropagationSimple/example.py
@@ -1,66 +1,6 @@
 # Importar las bibliotecas necesarias
 import numpy as np
-
-class NeuralNetwork:
-    def __init__(self, input_size, hidden_size, output_size):
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.output_size = output_size
-        
-        # Inicializar los pesos y los sesgos aleatoriamente
-        self.W1 = np.random.randn(self.input_size, self.hidden_size)
-        self.b1 = np.zeros((1, self.hidden_size))
-        self.W2 = np.random.randn(self.hidden_size, self.output_size)
-        self.b2 = np.zeros((1, self.output_size))
-        
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
-    
-    def sigmoid_derivative(self, x):
-        return x * (1 - x)
-    
-    def forward(self, X):
-        self.z1 = np.dot(X, self.W1) + self.b1
-        self.a1 = self.sigmoid(self.z1)
-        self.z2 = np.dot(self.a1, self.W2) + self.b2
-        self.a2 = self.sigmoid(self.z2)
-        return self.a2
-    
-    def backward(self, X, y, learning_rate):
-        m = X.shape[0]
-        
-        # Calcular las derivadas parciales del error con respecto a las activaciones
-        dZ2 = self.a2 - y
-        dW2 = (1 / m) * np.dot(self.a1.T, dZ2)
-        db2 = (1 / m) * np.sum(dZ2, axis=0, keepdims=True)
-        dZ1 = np.dot(dZ2, self.W2.T) * self.sigmoid_derivative(self.a1)
-        dW1 = (1 / m) * np.dot(X.T, dZ1)
-        db1 = (1 / m) * np.sum(dZ1, axis=0, keepdims=True)
-        
-        # Actualizar los pesos y sesgos
-        self.W2 -= learning_rate * dW2
-        self.b2 -= learning_rate * db2
-        self.W1 -= learning_rate * dW1
-        self.b1 -= learning_rate * db1
-        
-    def train(self, X, y, epochs, learning_rate):
-        for i in range(epochs):
-            # Paso de forward
-            predictions = self.forward(X)
-            
-            # Paso de backward
-            self.backward(X, y, learning_rate)
-            
-            # Calcular el error (loss)
-            loss = np.mean(-y * np.log(predictions) - (1 - y) * np.log(1 - predictions))
-            
-            # Imprimir el error en cada época
-            if i % 1000 == 0:
-                print(f'Época {i}, Error: {loss:.4f}')
-                
-    def predict(self, X):
-        return np.round(self.forward(X))
-
+from nnlib import NeuralNetwork
 
 # Crear datos de entrenamiento
 X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
